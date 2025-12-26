d0a98c2f6b5693df1e7f1e569365124dd54fd404
Ekaterina <ekaterinaehringhaus180@gmail.com>
2025-12-26 14:53:31 +0100

    show licence text in own window instead of tooltip

--- ui/screens/credit-screen/credit_element.gd
+++ ui/screens/credit-screen/credit_element.gd
@@ -1,18 +1,20 @@
 class_name CreditButton
 extends VBoxContainer
 
+signal display_license
+
 var credit: CreditEntry
 
 func set_credit(credit_entry: CreditEntry) -> void:
 	credit = credit_entry
 	if credit.descriptor:
 		$DescribtorLabel.text = credit.descriptor
-	$CreditButton.text = credit.name
-	
-	if credit.license:
-		$CreditButton.tooltip_text = credit.license
+	$HBoxContainer/CreditButton.text = credit.name
 	if credit.url:
-		$CreditButton.pressed.connect(open_url)
+		$HBoxContainer/CreditButton.pressed.connect(open_url)
 
 func open_url():
 	OS.shell_open(credit.url)
+
+func _on_license_button_pressed() -> void:
+	display_license.emit(credit)
